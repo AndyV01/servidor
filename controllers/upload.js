fc4b9a98320a1d320900cd64005a57fdd8bd6125
Andy Duff <andyduffdj25@gmail.com>
2022-03-26 17:49:00 -0300

    up

--- controllers/upload.js
+++ controllers/upload.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '../uploads')
+        cb(null, '/app/public/uploads')
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname)
